20aeb8420be4bfe65d4c8b9a57433d5d707461e1
Tianji Zhang <zhang.sh.tianji@gmail.com>
2022-06-13 22:18:17 -0400

    add functionality to placing player's piece of choice and reset btn

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
+
+/**
+ * Player factory function
+ * @param {*} name 
+ * @returns 
+ */
+ const player = (name, piece) => {
+    return { name, piece };
+}
+
 const gameBoard = (() => {
-    let board = ['X','X','X','X','X','X','X','X','X'];
+    let board = ['','','','','','','','',''];
+
+    resetBtn = document.getElementById('reset-btn');
+    cells = document.querySelectorAll('.cell')
+    resetBtn.addEventListener('click', () => {
+        for(let i = 0; i < board.length; i++) {
+            cells[i].textContent = '';
+            board[i] = '';
+        }
+    })
     return { board }
 })();
 
-const displayController = (board => {
-    cells = document.querySelectorAll('.cell')
+const p1 = player('Elon', 'X')
+
+const displayController = ((board, piece) => {
+    
+
+    for(let i = 0; i < board.length; i++) {
+        cells[i].addEventListener('click', (e) => {
+            if (board[i] == '') {
+                e.target.textContent = piece;
+                board[i] = piece;
+            }
+        })
+        // add hovering such that lighter colored piece shows where user hovers
+    }
+
     const drawGrid = () => {
         for(let i = 0; i < board.length; i++) {
             cells[i].textContent = board[i];
@@ -14,15 +46,6 @@ const displayController = (board => {
     return {
         drawGrid
     }
-})(gameBoard.board);
+})(gameBoard.board, p1.piece);
 
-/**
- * Player factory function
- * @param {*} name 
- * @returns 
- */
-const player = (name) => {
-    return { name };
-}
-
-displayController.drawGrid()
+// displayController.drawGrid()
